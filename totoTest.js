7dd816477b0f8160880d5adf10a57e9308de1290
mnawahda <mnawahda@asaltech.com>
2012-12-03 17:12:55 +0200

    tototest

---
+++ totoTest.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: mnawahda
+ * Date: 12/3/12
+ * Time: 5:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+       var x = function()
+{
+    alert("hello");
+};
