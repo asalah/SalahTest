6e4109f8627ee5031a44751a75ddc3300eb77b00
mnawahda <mnawahda@asaltech.com>
2012-12-03 17:32:00 +0200

    tototest

--- totoTest.js
+++ totoTest.js
@@ -5,7 +5,12 @@
  * Time: 5:02 PM
  * To change this template use File | Settings | File Templates.
  */
-       var x = function()
-{
-    alert("toooooooooooooot");
-};
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+
+}).listen(8124);
+
+console.log('Server running at http://127.0.0.1:8124/');
